4ced422f2e8868b4da251a8f9390cc1825c159dc
yushengwei <yushengwei@meicai.cn>
2020-01-03 18:37:05 +0800

    init

---
+++ src/main/java/com/yswsoft/leetcode/arithmetic/Main.java
@@ -0,0 +1,27 @@
+package com.yswsoft.leetcode.arithmetic;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Description: <br>
+ * Copyright: Copyright (c) 2016<br>
+ * Company: 北京云杉世界信息技术有限公司<br>
+ *
+ * @author yushengwei on 2020/1/3 10:36<br>
+ */
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,4,6,3,2,5,8,7,9,10,3,53,45,3,75,423,342,34,2,423,4,234,23,65,453,56};
+    }
+
+    //2分查找
+    private static void binarySearch(int[] arr,int key){
+        Arrays.binarySearch(arr,key);
+    }
+
+    //2分查找
+    private static void binarySearch(int[] arr,int start,int end,int key){
+
+    }
+}
